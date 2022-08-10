29dc083244f2289c2730d7eefba6d2f0ba228d16
Erik S. Carlsten <ecarlste@gmail.com>
2022-08-10 11:29:41 -0600

    Added several tests around new method

--- src/models/equipment.test.ts
+++ src/models/equipment.test.ts
@@ -1,7 +1,25 @@
-function sum(a: number, b: number) {
-  return a + b;
-}
+import Equipment from './equipment';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('hasNameLongerThan should return true when name longer than input', () => {
+  const equipment = new Equipment('name');
+
+  const result = equipment.hasNameLongerThan(1);
+
+  expect(result).toBe(true);
+});
+
+test('hasNameLongerThan should return false when name shorter than input', () => {
+  const equipment = new Equipment('name');
+
+  const result = equipment.hasNameLongerThan(10);
+
+  expect(result).toBe(false);
+});
+
+test('hasNameLongerThan should return false when name same length as input', () => {
+  const equipment = new Equipment('name');
+
+  const result = equipment.hasNameLongerThan(4);
+
+  expect(result).toBe(false);
 });
