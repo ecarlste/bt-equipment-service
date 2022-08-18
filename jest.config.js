a23a08317c2654f15a9fa681b64e49a67e1139ba
Erik S. Carlsten <ecarlste@gmail.com>
2022-08-18 13:26:11 -0600

    Added CircleCI test run job/workflow (#3)

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,15 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  modulePathIgnorePatterns: ['<rootDir>/dist'],
+  reporters: ['default', ['jest-junit', { outputDirectory: './reports/junit' }]],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,6 @@
 import dotenv from 'dotenv';
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-const envFound = dotenv.config();
-if (envFound.error) {
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
-}
+dotenv.config();
 
 export default {
   port: parseInt(process.env.PORT as string, 10),
