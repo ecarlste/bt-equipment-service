ee54f6f2669ea7abd732934bf6e2447de9c538da
Erik S. Carlsten <ecarlste@gmail.com>
2022-08-18 13:16:58 -0600

    Added jest coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,13 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/dist'],
-  reporters: ['default', ['jest-junit', { outputDirectory: './reports/junit' }]]
+  reporters: ['default', ['jest-junit', { outputDirectory: './reports/junit' }]],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
