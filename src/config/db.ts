e99e2bb7e1eb1a846cd750cec1f0a8194c5fa9e9
Erik S. Carlsten <ecarlste@gmail.com>
2022-08-19 16:29:19 -0600

    Added local dev docker via compose

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,33 +1,9 @@
 import mongoose from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server';
 import config from '.';
 export class db {
-  private static mongo: MongoMemoryServer;
-
   static async setUp(): Promise<void> {
-    const node_env = process.env.NODE_ENV;
-
-    if (node_env == 'development' || node_env == 'test') {
-      await db.connectToInMemoryDb();
-    } else {
-      await db.connectToRemoteDb();
-    }
-  }
-
-  private static async connectToInMemoryDb() {
-    db.mongo = await MongoMemoryServer.create();
-    const uri = db.mongo.getUri();
-
-    await db.connectToMongoDb(uri);
-  }
-
-  private static async connectToRemoteDb() {
-    await db.connectToMongoDb(config.databaseUri);
-  }
-
-  private static async connectToMongoDb(uri: string) {
     try {
-      await mongoose.connect(uri);
+      await mongoose.connect(config.databaseUri);
     } catch (error: unknown) {
       console.error(error);
       console.error('MongoDB failed to connect...');
@@ -35,21 +11,16 @@ export class db {
   }
 
   static async dropDatabase() {
-    if (db.mongo) {
-      await mongoose.connection.dropDatabase();
-      await mongoose.connection.close();
-      await db.mongo.stop();
-    }
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
   }
 
   static async dropCollections() {
-    if (db.mongo) {
-      const collections = mongoose.connection.collections;
+    const collections = mongoose.connection.collections;
 
-      for (const key in collections) {
-        const collection = collections[key];
-        await collection.deleteMany({});
-      }
+    for (const key in collections) {
+      const collection = collections[key];
+      await collection.deleteMany({});
     }
   }
 }
