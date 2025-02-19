0afd553e9d7adf94e9e7fe723e2f1473778a3887
Erik S. Carlsten <ecarlste@gmail.com>
2025-02-19 16:46:23 -0700

    added drizzle weapon table schema

---
+++ weapon/schema.ts
@@ -0,0 +1,32 @@
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  real,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+import { TechRating } from "../lib/TechRating";
+import { sql } from "drizzle-orm";
+
+const techRatingEnumValues = Object.values(TechRating) as [string, ...string[]];
+export const techRatingEnum = pgEnum("tech_rating", techRatingEnumValues);
+
+export const weapons = pgTable("", {
+  id: uuid().primaryKey().defaultRandom(),
+  name: text().unique().notNull(),
+  heat: text().notNull(),
+  damage: text().notNull(),
+  range: text().notNull(),
+  ammoPerTon: integer(),
+  weight: real().notNull(),
+  criticalSlots: integer().notNull(),
+  techRating: techRatingEnum().notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+    () => new Date()
+  ),
+});
