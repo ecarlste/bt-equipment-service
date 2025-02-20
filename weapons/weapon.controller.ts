37cb81df23b941ce41d25c2031d397e2b74490c6
Erik S. Carlsten <ecarlste@gmail.com>
2025-02-20 13:05:11 -0700

    add error handling to weapons controller (#4)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **Bug Fixes**
    - Enhanced error messaging across key operations, providing clearer
    notifications when issues occur during creation, retrieval, update, and
    deletion processes. These improvements help ensure that users receive
    more informative feedback, leading to a smoother and more predictable
    experience in handling weapon management operations.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- weapons/weapon.controller.ts
+++ weapons/weapon.controller.ts
@@ -1,4 +1,4 @@
-import { api } from "encore.dev/api";
+import { api, APIError } from "encore.dev/api";
 import {
   CreateWeaponDto,
   UpdateWeaponDto,
@@ -9,21 +9,33 @@ import WeaponService from "./weapon.service";
 export const create = api(
   { expose: true, method: "POST", path: "/weapons" },
   async (data: CreateWeaponDto): Promise<WeaponResponse> => {
-    return WeaponService.create(data);
+    try {
+      return await WeaponService.create(data);
+    } catch (error) {
+      throw APIError.aborted(error?.toString() || "Error creating weapon");
+    }
   }
 );
 
 export const readOne = api(
   { expose: true, method: "GET", path: "/weapons/:id" },
   async ({ id }: { id: string }): Promise<WeaponResponse> => {
-    return WeaponService.findOne(id);
+    try {
+      return await WeaponService.findOne(id);
+    } catch (error) {
+      throw APIError.aborted(error?.toString() || "Error getting weapon by ID");
+    }
   }
 );
 
 export const read = api(
   { expose: true, method: "GET", path: "/weapons" },
   async (): Promise<WeaponResponse> => {
-    return WeaponService.find();
+    try {
+      return await WeaponService.find();
+    } catch (error) {
+      throw APIError.aborted(error?.toString() || "Error getting weapons data");
+    }
   }
 );
 
@@ -36,13 +48,21 @@ export const update = api(
     id: string;
     data: UpdateWeaponDto;
   }): Promise<WeaponResponse> => {
-    return WeaponService.update(id, data);
+    try {
+      return await WeaponService.update(id, data);
+    } catch (error) {
+      throw APIError.aborted(error?.toString() || "Error updating weapon");
+    }
   }
 );
 
 export const destroy = api(
   { expose: true, method: "DELETE", path: "/weapons/:id" },
   async ({ id }: { id: string }): Promise<WeaponResponse> => {
-    return WeaponService.delete(id);
+    try {
+      return await WeaponService.delete(id);
+    } catch (error) {
+      throw APIError.aborted(error?.toString() || "Error deleting weapon");
+    }
   }
 );
