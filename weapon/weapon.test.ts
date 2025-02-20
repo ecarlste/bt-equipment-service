a1ad97468098bc6da433b97c647df8585ffc49f8
Erik S. Carlsten <ecarlste@gmail.com>
2025-02-20 09:03:12 -0700

    implement all weapon API endpoints

--- weapon/weapon.test.ts
+++ weapon/weapon.test.ts
@@ -1,15 +1,92 @@
-import { describe, expect, test } from "vitest";
-import { defaultWeapon, get } from "./weapon";
+import { afterAll, afterEach, beforeAll, describe, expect, it } from "vitest";
+import { create, get, list, remove } from "./weapon";
+import { CreateWeaponDto, WeaponDto } from "./weapon.interface";
+import { TechRating } from "../lib/TechRating";
+import { db } from "./database";
+import { weapons } from "./schema";
 
-describe("get", () => {
-  test("should combine string with parameter value", async () => {
-    const resp = await get({ id: "fake-weapon-id" });
+const defaultTestWeapon: CreateWeaponDto = {
+  name: "Test Weapon",
+  heat: "3",
+  damage: "5",
+  range: "0/3/6/9",
+  ammoPerTon: null,
+  weight: 1,
+  criticalSlots: 1,
+  techRating: TechRating.CommonTech,
+};
 
-    expect(resp).toStrictEqual({
-      result: {
-        id: "fake-weapon-id",
-        ...defaultWeapon,
-      },
+const weaponToGet: CreateWeaponDto = {
+  ...defaultTestWeapon,
+  name: "Weapon Get Test",
+};
+
+const weaponToCreate: CreateWeaponDto = {
+  ...defaultTestWeapon,
+  name: "Weapon Create Test",
+};
+
+describe("weapon", () => {
+  beforeAll(async () => {
+    await db.delete(weapons);
+  });
+
+  afterEach(async () => {
+    await db.delete(weapons);
+  });
+
+  describe("create", () => {
+    it("should return the weapon created with an id", async () => {
+      const weaponCreated = await create(weaponToCreate);
+
+      const { id } = weaponCreated.result as WeaponDto;
+      expect(weaponCreated.result).toMatchObject({
+        id,
+        ...weaponToCreate,
+      });
+    });
+  });
+
+  describe("get", () => {
+    it("should return the weapon found by id", async () => {
+      const [weapon] = await db.insert(weapons).values(weaponToGet).returning();
+
+      const weaponFound = await get({ id: weapon.id });
+
+      expect(weaponFound.result).toMatchObject(weapon);
+    });
+  });
+
+  describe("list", () => {
+    it("should return all weapons", async () => {
+      const weaponsToList: CreateWeaponDto[] = [
+        { ...defaultTestWeapon, name: "Weapon List Test 1" },
+        { ...defaultTestWeapon, name: "Weapon List Test 2" },
+      ];
+      await db.insert(weapons).values(weaponsToList).returning();
+
+      const weaponsFound = await list();
+
+      expect(weaponsFound.result).toMatchObject(weaponsToList);
+    });
+  });
+
+  describe("delete", () => {
+    it("should delete the weapon found by id", async () => {
+      const [weapon] = await db.insert(weapons).values(weaponToGet).returning();
+
+      await remove({ id: weapon.id });
+
+      const weaponsFound = await list();
+      expect(weaponsFound.result).toEqual([]);
+    });
+
+    it("should return the weapon deleted", async () => {
+      const [weapon] = await db.insert(weapons).values(weaponToGet).returning();
+
+      const weaponDeleted = await remove({ id: weapon.id });
+
+      expect(weaponDeleted.result).toMatchObject(weapon);
     });
   });
 });
--- weapon/weapon.ts
+++ weapon/weapon.ts
@@ -1,19 +1,8 @@
 import { api } from "encore.dev/api";
-import { TechRating } from "../lib/TechRating";
+import { eq } from "drizzle-orm";
 import { db } from "./database";
 import { weapons } from "./schema";
-import { CreateWeaponDto, WeaponDto, WeaponResponse } from "./weapon.interface";
-
-export const defaultWeapon = {
-  name: "Medium Laser",
-  heat: "3",
-  damage: "5",
-  range: "0/3/6/9",
-  ammoPerTon: null,
-  weight: 1,
-  criticalSlots: 1,
-  techRating: TechRating.CommonTech,
-} satisfies Omit<WeaponDto, "id">;
+import { CreateWeaponDto, WeaponResponse } from "./weapon.interface";
 
 export const create = api(
   { expose: true, method: "POST", path: "/weapon" },
@@ -27,22 +16,33 @@ export const create = api(
 export const get = api(
   { expose: true, method: "GET", path: "/weapon/:id" },
   async ({ id }: { id: string }): Promise<WeaponResponse> => {
-    const resp = {
-      result: {
-        id,
-        ...defaultWeapon,
-      },
-    } satisfies WeaponResponse;
+    const [weapon] = await db
+      .select()
+      .from(weapons)
+      .where(eq(weapons.id, id))
+      .limit(1);
 
-    return resp;
+    return { result: weapon };
   }
 );
 
 export const list = api(
   { expose: true, method: "GET", path: "/weapon" },
   async (): Promise<WeaponResponse> => {
-    const result = await db.select().from(weapons);
+    const weaponList = await db.select().from(weapons);
 
-    return { result };
+    return { result: weaponList };
+  }
+);
+
+export const remove = api(
+  { expose: true, method: "DELETE", path: "/weapon/:id" },
+  async ({ id }: { id: string }): Promise<WeaponResponse> => {
+    const [weapon] = await db
+      .delete(weapons)
+      .where(eq(weapons.id, id))
+      .returning();
+
+    return { result: weapon };
   }
 );
